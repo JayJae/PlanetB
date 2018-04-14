7d2a2905bc9b616c06b42f3bf1543c3c6b441f56
jayjae <wigglegigglegoggle@gmail.com>
2018-04-14 22:05:34 +0900

    Add page scroll for main page

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,36 @@
 import React, { Component } from 'react';
+import {SectionsContainer, Section, Header, Footer} from 'react-fullpage';
 
 export default class App extends Component {
   render() {
+    let options = {
+      sectionClassName:     'section',
+      anchors:              ['home', 'aboutUs', 'teamMember', 'contactUs'],
+      scrollBar:            false,
+      navigation:           true,
+      verticalAlign:        false,
+      sectionPaddingTop:    '50px',
+      sectionPaddingBottom: '50px',
+      arrowNavigation:      true,
+      offsetSections:       false,
+    };
+
     return (
-      <div>React simple starter</div>
+      <div md={12} lg={12} xs={12}>
+        <Header>
+          <a href="#home">Planet B</a>
+          <a href="#aboutUs">About Us</a>
+          <a href="#teamMember">Team Member</a>
+          <a href="#contactUs">Contact Us</a>
+        </Header>
+
+        <SectionsContainer {...options}>
+          <Section color="#FFF">Page 1</Section>
+          <Section color="#123">Page 2</Section>
+          <Section color="#456">Page 3</Section>
+          <Section color="#456">Page 4</Section>
+        </SectionsContainer>
+      </div>
     );
   }
 }
