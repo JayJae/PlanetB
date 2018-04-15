9e25e37035f5eab2c1c98e0099c61d9414fce07f
jayjae <wigglegigglegoggle@gmail.com>
2018-04-16 01:10:11 +0900

    Add parameters to improve particles animation

--- src/components/PlanetBMain.jsx
+++ src/components/PlanetBMain.jsx
@@ -2,9 +2,33 @@ import React from 'react';
 import Particles from 'react-particles-js';
 
 const PlanetBMain = () => {
+  const params = {
+    particles: {
+      line_linked: {
+        enable: true
+      },
+      number: {
+        value: 200
+      }
+    },
+    interactivity: {
+      detect_on: "window",
+      events: {
+        onhover: {
+          enable: true,
+          mode: "grab"
+        },
+        onclick: {
+          enable: true,
+          mode: "repulse"
+        }
+      }
+    }
+  };
+
   return (
     <div className="planetb-main">
-      <Particles className="particles"/>
+      <Particles params={params} className="particles"/>
       <span className="titles">
         <h1 className="maintitle">PlanetB</h1>
         <h2 className="subtitle">Planet Blockchain, the Postech Blockchain Group</h2>
