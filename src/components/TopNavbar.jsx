b0d703af4d8145a307f81042612a69a78905d66f
jayjae <wigglegigglegoggle@gmail.com>
2018-04-16 00:15:23 +0900

    Add links for menu items

--- src/components/TopNavbar.jsx
+++ src/components/TopNavbar.jsx
@@ -2,39 +2,26 @@ import React, { Component } from 'react';
 import { Jumbotron, Button } from 'react-bootstrap';
 import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 
-class TopNavbar extends React.Component {
+export default class TopNavbar extends Component {
   render() {
     return (
-	<Navbar inverse collapseOnSelect fixedTop>
-	  <Navbar.Header>
-	    <Navbar.Brand>
-	      <a href="#Home"><img src="./src/images/logo_white.png" width="auto" height="100%" /></a>
-	    </Navbar.Brand>
-	    <Navbar.Toggle />
-	  </Navbar.Header>
-	  <Navbar.Collapse>
-	    <Nav pullRight>
-	      <NavItem eventKey={1} href="#">
-		ABOUT
-	      </NavItem>
-	      <NavItem eventKey={2} href="#">
-		RESEARCH
-	      </NavItem>
-	      <NavItem eventKey={3} href="#">
-		MEMBER
-	      </NavItem>
-	      <NavItem eventKey={4} href="#">
-		EVENT
-	      </NavItem>
-	      <NavItem eventKey={5} href="#">
-		CONTACT
-	      </NavItem>
-	    </Nav>
-	  </Navbar.Collapse>
-	</Navbar>
+    	<Navbar inverse collapseOnSelect fixedTop>
+    	  <Navbar.Header>
+    	    <Navbar.Brand>
+    	      <a href="#ABOUT"><img src="./src/images/logo_white.png" width="auto" height="100%" /></a>
+    	    </Navbar.Brand>
+    	    <Navbar.Toggle />
+    	  </Navbar.Header>
+    	  <Navbar.Collapse>
+    	    <Nav pullRight>
+    	      <NavItem eventKey={1} href="#ABOUT">ABOUT</NavItem>
+    	      <NavItem eventKey={2} href="#RESEARCH">RESEARCH</NavItem>
+    	      <NavItem eventKey={3} href="#MEMBER">MEMBER</NavItem>
+    	      <NavItem eventKey={4} href="#EVENT">EVENT</NavItem>
+    	      <NavItem eventKey={5} href="#CONTACT">CONTACT</NavItem>
+    	    </Nav>
+    	  </Navbar.Collapse>
+    	</Navbar>
     );
   }
 }
-
-
-export default TopNavbar;
