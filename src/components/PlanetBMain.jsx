0b19dbcb8448ab7d5157ddfee543443d960203b2
jayjae <wigglegigglegoggle@gmail.com>
2018-04-16 00:17:51 +0900

    Add particles-js animation for PlanetB main page

---
+++ src/components/PlanetBMain.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Particles from 'react-particles-js';
+
+const PlanetBMain = () => {
+  return (
+    <div className="planetb-main">
+      <Particles className="particles"/>
+      <span className="titles">
+        <h1 className="maintitle">PlanetB</h1>
+        <h2 className="subtitle">Planet Blockchain, the Postech Blockchain Group</h2>
+      </span>
+    </div>
+  );
+};
+
+export default PlanetBMain;
