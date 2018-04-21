665672fd854f0e9c3af06d5c4cce76b1fb1fcea7
Ki young Ju <kiyoungju@Kiui-MacBook-Pro.local>
2018-04-21 18:10:07 +0900

    add team intro page

--- src/components/Member.jsx
+++ src/components/Member.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
-const Member = ({imageUrl, name, description}) => {
+const Member = ({imageUrl, name, description, linkedin}) => {
   return (
-    <div className="profile col-md-3">
+    <div className="profile col-md-2">
       <div className="thumb">
-        <img src={imageUrl} className="profile-img">
-          <div className="profile-text">
-            <h3>{name}</h3>
-            <p>{description}</p>
-          </div>
-        </img>
+          <img src={imageUrl} className="profile-img">
+              <div className="profile-text">
+                <h3> {name}</h3>
+                <p>{description}</p>
+              </div>
+          </img>
       </div>
     </div>
   );
--- src/components/MemberList.jsx
+++ src/components/MemberList.jsx
@@ -4,49 +4,70 @@ import Member from './Member';
 const MemberList = () => {
   const memberInfos = [
     {
-      imageUrl: "/src/images/team/gyjoo.png",
-      name: "Ki Young Ju",
-      description: "Founder. Product Manager at theloop"
-    },
-    {
       imageUrl: "/src/images/team/dylee.png",
       name: "Do yeop Lee",
-      description: "PhD Candidate. Kakao Brain"
+      description: "PhD Candidate. Kakao Brain",
     },
     {
-      imageUrl: "/src/images/team/hjpark.png",
+      imageUrl: "/src/images/team/hjpark.jpg",
       name: "Hyun Jae Park",
       description: "DApp Developer @ Penta Security Systems"
     },
     {
-      imageUrl: "",
+      imageUrl: "/src/images/team/kjlee.jpg",
       name: "Kyung June Lee",
-      description: "PhD Candidate"
+      description: "PhD Candidate, Hardware AI Chip, Co-founder of Hozaebox"
     },
     {
-      imageUrl: "https://pbs.twimg.com/profile_images/548619278333652993/wr4d4F0X_400x400.jpeg",
-      name: "오징어",
-      description: "나는 오징어다"
+      imageUrl: "/src/images/team/gyjoo.png",
+      name: "Ki Young Ju",
+      description: "Founder of Planet.B Product Manager at theloop",
+      linkedin: "https://www.linkedin.com/in/ki-young-ju/"
     },
     {
-      imageUrl: "https://pbs.twimg.com/profile_images/548619278333652993/wr4d4F0X_400x400.jpeg",
-      name: "오징어",
-      description: "나는 오징어다"
+      imageUrl: "/src/images/team/hjjoo.jpg",
+      name: "Hyo Jin Ju",
+      description: "Electriacl Eng. Undergrad"
     },
     {
-      imageUrl: "https://pbs.twimg.com/profile_images/548619278333652993/wr4d4F0X_400x400.jpeg",
-      name: "오징어",
-      description: "나는 오징어다"
+      imageUrl: "/src/images/team/bjson.jpg",
+      name: "Beom Jun Sohn",
+      description: "Creative IT Eng. Undergrad"
     },
     {
-      imageUrl: "https://pbs.twimg.com/profile_images/548619278333652993/wr4d4F0X_400x400.jpeg",
-      name: "오징어",
-      description: "나는 오징어다"
+      imageUrl: "/src/images/team/hjhjpark.jpg",
+      name: "Hyung Jun Park",
+      description: "MS Candidate, Computer Science @ POSTECH"
     },
     {
-      imageUrl: "https://pbs.twimg.com/profile_images/548619278333652993/wr4d4F0X_400x400.jpeg",
-      name: "오징어",
-      description: "나는 오징어다"
+      imageUrl: "/src/images/team/jwpark.jpg",
+      name: "Jin Woo Park",
+      description: "Product Manager @ Chain Partners"
+    },
+    {
+      imageUrl: "/src/images/team/jycho.jpg",
+      name: "Jae Yeon Cho",
+      description: "ICO Project Manager @ Penta Security Systems"
+    },
+    {
+      imageUrl: "/src/images/team/tyjeong.png",
+      name: "Tae Yeol Jeong",
+      description: "PhD, CS POSTECH, Fog Computing & Distributed Storage Systems"
+    },
+    {
+      imageUrl: "/src/images/team/bkjang.jpg",
+      name: "Byung Kook Jang",
+      description: "Industrial & Management Eng. Crypto Millianaire"
+    },
+    {
+      imageUrl: "/src/images/team/bjkim.jpg",
+      name: "Byung Jun Kim",
+      description: "PhD, EE POSTECH, Hardware AI Chip, Co-founder of Hozaebox"
+    },
+    {
+      imageUrl: "/src/images/team/kwjoo.jpg",
+      name: "Kyung Woon Ju",
+      description: "Industrial & Management Eng., Undergrad"
     }
   ]
 
@@ -55,7 +76,8 @@ const MemberList = () => {
       <Member
       imageUrl={memberInfo.imageUrl}
       name={memberInfo.name}
-      description={memberInfo.description} />
+      description={memberInfo.description}
+      linkedin={memberInfo.linkedin}/>
     );
   });
 
